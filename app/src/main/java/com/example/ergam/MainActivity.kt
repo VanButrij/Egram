c39df2f510e2bb8717d92705cb6c5d613bb2f4c5
butri <butrij.iw@yandex.ru>
2023-01-06 07:07:35 +0300

    25 lesson

---
+++ app/src/main/java/com/example/ergam/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.ergam
+
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.widget.Toolbar
+import com.example.ergam.activities.RegisterActivity
+import com.example.ergam.databinding.ActivityMainBinding
+import com.example.ergam.models.User
+import com.example.ergam.ui.fragments.ChatsFragment
+import com.example.ergam.ui.objects.AppDrawer
+import com.example.ergam.utilits.*
+import com.google.firebase.storage.StorageReference
+import com.theartofdev.edmodo.cropper.CropImage
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    lateinit var mAppDrawer: AppDrawer
+    private lateinit var mToolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        APP_ACTIVITY = this
+        initFirebase()
+        initUser{
+            initFields()
+            initFunc()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        AppStates.updateState(AppStates.ONLINE)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        AppStates.updateState(AppStates.OFFLINE)
+    }
+
+    private fun initFunc() {
+        if (AUTH.currentUser != null) {
+            setSupportActionBar(mToolbar)
+            mAppDrawer.create()
+            replaceFragment(ChatsFragment(), false)
+        } else {
+            replaceActivity(RegisterActivity())
+        }
+    }
+
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
+        mAppDrawer = AppDrawer(this, mToolbar)
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/ergam/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.ergam.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.ergam.R
+
+
+class ChatsFragment : Fragment(R.layout.fragment_chats) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
